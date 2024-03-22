d5b4774737218c75b9f1503590246b34b0910f26
Yulia <yuliaolegyureva@gmail.com>
2024-03-23 01:25:48 +0700

    empty

--- app/keyboard.py
+++ app/keyboard.py
@@ -1,27 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-# main = ReplyKeyboardMarkup(keyboard=[
-#     [KeyboardButton(text='btn1')],
-#     [KeyboardButton(text='btn2'), KeyboardButton(text='btn3')]
-# ],
-#     resize_keyboard=True,
-#     input_field_placeholder='choose')
-#
-# settings = InlineKeyboardMarkup(inline_keyboard=[
-#     [InlineKeyboardButton(text='pinterest', url='https://pin.it/78jOQY9AX')]
-# ])
-#
-# cars = ['car1', 'car2', 'car3', 'car4']
-#
-#
-# async def inline_cars():
-#     keyboard = InlineKeyboardBuilder()
-#     for car in cars:
-#         keyboard.add(InlineKeyboardButton(text=car, url='https://youtube.com/'))
-#     return keyboard.adjust(2).as_markup()
-
-
 food_courts = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='RedRabbit🐇', callback_data='red-rabbit'),
      InlineKeyboardButton(text='Хан Буз♨️', callback_data='khan-buz')]
