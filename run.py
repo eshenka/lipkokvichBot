6906c6ab8f8f1c4476752136fc99efd3f3d0e2f4
Alexander <a.tyurin@g.nsu.ru>
2024-03-26 18:04:20 +0700

    removed unnecessary imports

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-import aiogram
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher
 
 from config import TOKEN
 from app.handlers import router
