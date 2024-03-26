44b8f85803c6eea3fb72b784609a8c9216879865
Yulia <yuliaolegyureva@gmail.com>
2024-03-27 00:39:54 +0700

    added easy menu writer, game and sticker features

--- app/handlers.py
+++ app/handlers.py
@@ -19,6 +19,38 @@ async def command_help(message: Message):
     await message.answer('mne bi kto pomog')
 
 
+@router.message(Command('play'))
+async def command_play(message: Message):
+    keyboards.game_numbers[message.from_user.id] = random.randint(0, 9_999_999)
+
+    await message.answer('Сколько раз БякБяк бякбякал, чтобы получить пятерку на экзамене?')
+    await message.answer('Подсказка! От 0 до 10 000 000 раз')
+
+
+@router.message(F.text.regexp(r'[0-9]'))
+async def game_number(message: Message):
+    user_id = message.from_user.id
+    user_number = keyboards.game_numbers[user_id]
+    input_number = int(message.text)
+
+    if user_number == input_number:
+        await message.answer('Ты угадал!!!')
+        keyboards.game_numbers[user_id] = random.randint(0, 9_999_999)
+
+    if input_number < user_number:
+        await message.answer('Больше')
+
+    if user_number < input_number:
+        await message.answer('Меньше')
+
+
+@router.message(F.content_type.in_({'sticker'}))
+async def send_sticker(message: Message):
+    sticker_pack = keyboards.stickers
+    pack_len = len(sticker_pack)
+    await message.answer_sticker(sticker=sticker_pack[random.randint(0, pack_len - 1)])
+
+
 @router.message(F.text == 'как дела?')
 async def text_how_are_you(message: Message):
     await message.answer('im fine')
@@ -38,8 +70,9 @@ async def red_rabbit_menu(callback: CallbackQuery):
 @router.callback_query(F.data.in_(keyboards.red_rabbit_menu_positions.keys()))
 async def red_rabbit_positions(callback: CallbackQuery):
     menu = keyboards.red_rabbit_menu_positions
+
     if callback.data in menu:
-        await callback.message.answer(menu[callback.data])
+        await callback.message.answer(text=keyboards.process_food_data(menu[callback.data]))
     else:
         raise Exception("invalid callback data")
 
@@ -58,8 +91,9 @@ async def khan_buz_menu(callback: CallbackQuery):
 @router.callback_query(F.data.in_(keyboards.khan_buz_menu_positions.keys()))
 async def khan_buz_positions(callback: CallbackQuery):
     menu = keyboards.khan_buz_menu_positions
+
     if callback.data in menu:
-        await callback.message.answer(menu[callback.data])
+        await callback.message.answer(text=keyboards.process_food_data(menu[callback.data]))
     else:
         raise Exception("invalid callback data")
 
